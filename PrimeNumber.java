9de262418c070fa8d79fac8d0b3106e416219234
Keerthi527 <48671102+Keerthi527@users.noreply.github.com>
2021-09-28 22:16:49 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class PrimeNumber
+{
+    public static void main(String[] args)
+ {
+        int counter = 0;
+        int primeNum = 0;
+        for (int num = 2; num < 10000000; num++) 
+	{
+            boolean isPrime = true;
+            for (int factor = 2; factor < num; factor++) 
+	    {
+                if (num % factor == 0)
+		{
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primeNum = num;
+                counter++;
+            }
+            if (counter == 10001) {
+                break;
+            }
+        }
+        System.out.println(primeNum);
+    }
+}
